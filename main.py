a9373af9bbe99dbaf4d37fd257a5f289241def26
Lin-Bu <lin.bucheim@gmail.com>
2025-01-07 20:37:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+%load_ext autoreload
+%autoreload 2
 
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+
+import pandas as pd
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+df.head()
+
+import seaborn as sns
+features = ['DFA', 'PPE']
+target = 'status'
+x = df[features]
+y = df[target]
+
+from sklearn.preprocessing import MinMaxScaler
+min_max_scaler = MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, df[target], test_size=0.2)
+
+from sklearn.svm import SVC
+model = SVC()
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
